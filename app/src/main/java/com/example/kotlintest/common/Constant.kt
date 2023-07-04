f0d0d14466a7fc8ab5cc4b248f95f63d4272ac0d
daejong <daejong@dkitec.com>
2023-07-04 15:32:12 +0900

    [MainActivity] 통신 및 Fragment 호출 버튼 추가.

--- app/src/main/java/com/example/kotlintest/common/Constant.kt
+++ app/src/main/java/com/example/kotlintest/common/Constant.kt
@@ -2,4 +2,6 @@ package com.example.kotlintest.common
 
 object Constant {
     const val BASE_URL = "https://jsonkeeper.com/b/"
+
+    const val CODE_SUCCESS = "0000"
 }
--- app/src/main/java/com/example/kotlintest/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/ui/activity/MainActivity.kt
@@ -4,10 +4,17 @@ import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlintest.R
 import com.example.kotlintest.base.BaseActivity
+import com.example.kotlintest.common.Constant
 import com.example.kotlintest.databinding.ActivityMainBinding
 import com.example.kotlintest.extension.loadImg
 import com.example.kotlintest.network.model.CustomList
+import com.example.kotlintest.network.model.RetrofitModel
+import com.example.kotlintest.network.retrofit.ApiManager
+import com.example.kotlintest.util.GLog
 import com.example.kotlintest.viewmodel.RetrofitViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var viewModel: RetrofitViewModel
@@ -18,40 +25,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun initView() {
-        binding.btnTest.setOnClickListener {
+        binding.btnToFragment.setOnClickListener {
             intent = Intent(mActivity, HostFragActivity::class.java)
             startActivity(intent)
 
-//            ApiManager.getThumbnail()?.enqueue(object : Callback<RetrofitModel> {
-//
-//                override fun onResponse(call: Call<RetrofitModel>, response: Response<RetrofitModel>) {
-//                    if (response.isSuccessful) {
-//                        val model: RetrofitModel? = response.body()
-//                        model?.let {
-//                            val errMsg: String? = model.errMsg
-//
-//                            when (model.code) {
-//                                "0000" -> {
-//                                    model.data?.let {
-//                                        updateViewModel(it.customList)
-//                                    } ?: run {
-//                                        GLog.e("data is null")
-//                                    }
-//                                }
-//                                else -> {
-//                                    GLog.e("실패 : $errMsg")
-//                                }
-//                            }
-//                        } ?: run {
-//                            GLog.e("model is null")
-//                        }
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<RetrofitModel>, t: Throwable) {
-//                    GLog.d("onFailure")
-//                }
-//            })
+            binding.btnRetrofit.setOnClickListener {
+                requestApi()
+            }
         }
     }
 
@@ -68,7 +48,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 }
 
                 mainTitle?.let {
-                    binding.btnTest.text = mainTitle
+                    binding.btnRetrofit.text = mainTitle
                 }
             }
         }
@@ -78,5 +58,39 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         if (!list.isNullOrEmpty()) {
             viewModel.updateListItem(list)
         }
+    }
+
+    private fun requestApi() {
+        ApiManager.getThumbnail()?.enqueue(object : Callback<RetrofitModel> {
+            override fun onResponse(call: Call<RetrofitModel>, response: Response<RetrofitModel>) {
+                if (response.isSuccessful) {
+                    val model: RetrofitModel? = response.body()
+                    model?.let {
+                        val errMsg: String? = model.errMsg
+
+                        when (model.code) {
+                            Constant.CODE_SUCCESS -> {
+
+                                model.data?.let {
+                                    updateViewModel(it.customList)
+
+                                } ?: run {
+                                    GLog.e("data is null")
+                                }
+                            }
+                            else -> {
+                                GLog.e("실패 : $errMsg")
+                            }
+                        }
+                    } ?: run {
+                        GLog.e("model is null")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<RetrofitModel>, t: Throwable) {
+                GLog.d("onFailure")
+            }
+        })
     }
 }
