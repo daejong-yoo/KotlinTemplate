df9e3fe8dce8986f8e5ddd0af975f23fdf279eec
daejong <daejong@dkitec.com>
2023-11-15 22:38:03 +0900

    [Util] dpToPx, pxToDp 변환 메소드 커밋

--- app/src/main/java/com/example/kotlintemplate/util/Util.kt
+++ app/src/main/java/com/example/kotlintemplate/util/Util.kt
@@ -1,8 +1,12 @@
 package com.example.kotlintemplate.util
 
+import android.content.Context
+import android.content.res.Resources
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import android.util.DisplayMetrics
+
 
 object Util {
 
@@ -14,5 +18,19 @@ object Util {
         }
 
         return text
+    }
+
+    //dp를 px로 변환 (dp를 입력받아 px을 리턴)
+    fun dpToPx(context: Context, dp: Int): Int {
+        val resources: Resources = context.getResources()
+        val metrics = resources.displayMetrics
+        return (dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
+    }
+
+    //px을 dp로 변환 (px을 입력받아 dp를 리턴)
+    fun pxToDp(context: Context, px: Int): Int {
+        val resources: Resources = context.getResources()
+        val metrics = resources.displayMetrics
+        return (px / (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
     }
 }
