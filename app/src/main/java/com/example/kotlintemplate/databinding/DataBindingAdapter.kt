954eabf1595f5da499166e99c9de4746eee280e1
daejong <daejong@dkitec.com>
2024-11-06 10:10:26 +0900

    - 사용하지 않는 주석 삭제.

--- app/src/main/java/com/example/kotlintemplate/databinding/DataBindingAdapter.kt
+++ app/src/main/java/com/example/kotlintemplate/databinding/DataBindingAdapter.kt
@@ -1,14 +1,3 @@
-/*
- * 5G eSportsLive version 01.00.01
- *
- *  Copyright �� 2019 kt corp. All rights reserved.
- *
- *  This is a proprietary software of kt corp, and you may not use this file except in
- *  compliance with license agreement with kt corp. Any redistribution or use of this
- *  software, with or without modification shall be strictly prohibited without prior written
- *  approval of kt corp, and the copyright notice above does not evidence any actual or
- *  intended publication of such software.
- */
 package com.example.kotlintemplate.databinding
 
 import android.widget.ImageView
